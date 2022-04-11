572da5a90c5468c9d2ca643fc8787d9a33809c22
leonardocarlas <lio.del.bronx@gmail.com>
2022-04-11 22:14:57 +0200

    Added Routing

--- utils/posts.ts
+++ utils/posts.ts
@@ -43,7 +43,21 @@ export function getSortedPostsData() {
 }
 
 export function getAllPostIds() {
+
   const fileNames = fs.readdirSync(postsDirectory)
+    // Returns an array that looks like this:
+    // [
+    //   {
+    //     params: {
+    //       id: 'ssg-ssr'
+    //     }
+    //   },
+    //   {
+    //     params: {
+    //       id: 'pre-rendering'
+    //     }
+    //   }
+    // ]
   return fileNames.map(fileName => {
     return {
       params: {
