31429aa76a91d22f69ffc2447d800f5bf50180ae
Leonardo Carlassare <lio.del.bronx@gmail.com>
2022-05-19 23:00:10 +0200

    Responsive overlay card

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -1,8 +1,22 @@
+import styles from './Services.module.scss';
+
 export default function Services() {
     
     return (
-        <div>
-            <p>Services</p>
-        </div>
+        <>
+            <div className={styles.bannerRed}>
+                <p>SERVICES</p>
+                
+            </div>
+            
+            <div className={styles.bannerBlue}>
+                <div className={styles.container}>
+                    <div className={styles.box}>CIAO</div>
+                    <div className={styles.box}>CIAO</div>
+                    <div className={styles.box}>CIAO</div>
+                </div>
+            </div>
+        
+        </>
     );
 }
