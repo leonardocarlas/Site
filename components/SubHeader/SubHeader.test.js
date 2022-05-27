bed10f95a2a42438b2705a6c97eac4bf6572fa9a
Leonardo Carlassare <lio.del.bronx@gmail.com>
2022-05-27 19:30:27 +0200

    Cleaned tests

--- components/SubHeader/SubHeader.test.js
+++ components/SubHeader/SubHeader.test.js
@@ -1,11 +1,7 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import AddMore from './AddMore';
 
-test('It should call the function handler on click', async () => {
-    const fn = jest.fn();
-    render(<AddMore onAdd={fn} />);
+describe("SubHeader Component", () => {
 
-    fireEvent.click(screen.getByTestId('iconButton'));
 
-    expect(fn).toHaveBeenCalled();
+
 });
--- pages/contact/Contact.test.js
+++ pages/contact/Contact.test.js
@@ -2,20 +2,8 @@ import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
 
 
-describe("Contact Page", () => {
 
-    it("should render the page", () => {
-        render(
-            <BrowserRouter>
-                <Provider store={store}>
-                    <Contact />
-                </Provider>
-            </BrowserRouter>
-        );
-    
-        const el = screen.getByAltText('coffee-image');
-        expect(el).toBeInTheDocument();
-    });
+describe("Contact Page", () => {
 
     
 
